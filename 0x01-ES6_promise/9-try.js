71dc73a81e753a558f54f1d5f565b413b0cdad6a
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-12 20:02:59 +0000

    tryalal

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,9 +4,10 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(err.toString());
+    queue.push(String(err));
   } finally {
-    queue.push("Guardrail was processed");
+    queue.push('Guardrail was processed');
   }
+
   return queue;
 }
